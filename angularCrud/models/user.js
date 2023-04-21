13a1f3bd7b3a7380044b4fa95146d8ae9e9554ec
Rishabh tiwari <105351344+rishabhtiwari2023@users.noreply.github.com>
2023-04-21 20:45:14 +0530

    Delete angularCrud directory

--- angularCrud/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-let userSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
-    name: String,
-    eamil: String,
-    address: String,
-    password: String,
-})
-module.exports = mongoose.model('users', userSchema)
